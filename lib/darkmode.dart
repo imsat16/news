e52297fb70c3e526c59ff3ed99f1413442338f54
ogi <ogirahmansyah96@gmail.com>
2023-06-08 16:54:58 +0700

    local commit

---
+++ lib/darkmode.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({key ? key}) : super(key: key);
+
+@override
+State<MyApp> createState() => _MyAppState();
+}
+
+bool _iconBool = false;
+
+IconData _iconLight = Icon.wb_sunny;
+IconData _iconDark = Icon.nights_stay;
+
+ThemeData _lightTheme = ThemeData(
+  primarySwatch: Colors.amber,
+  brightness: Brightness.light,
+  buttonBarTheme: const ButtonThemeData(
+    buttonColor: Colors.amberAccent)
+);
+
+ThemeData _darkTheme = ThemeData(
+  primarySwatch: Colors.red,
+   brightness: Brightness.dark,
+);
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: _iconBool ? _darkTheme : _lightTheme,
+      home:Scaffold(
+        appBar: AppBar(
+          title: const Text("Dark & Light Theme"),
+          actions: [
+            IconButton(
+              onPressed: () {
+                setState(() {
+                  _iconBool = !_iconBool;
+                });
+              },
+              icon: Icon(_iconBool ? _iconDark : _iconLight),
+              ),
+          ],
+        ),
+        body: Center(
+          child: RaisedButton(
+            onPressed: () {},
+            child: const Text("hello")
+            ),
+          ),
+      ),
+    );
+  }
+}
